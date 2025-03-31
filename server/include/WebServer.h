f7006d2dc43f1f376112c45888297ccb509d5ceb
Ligfi <149589876+Lev2011-karcev@users.noreply.github.com>
2025-03-31 22:08:26 +0500

    Update WebServer.h

--- server/include/WebServer.h
+++ server/include/WebServer.h
@@ -1,1 +1,69 @@
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <string>
+#include <memory>
+#include <functional>
 
+
+namespace ssl = boost::asio::ssl;
+using boost::asio::ip::tcp;
+using boost::system::error_code;
+
+
+class HttpsWebServer {
+public :
+
+	HttpsWebServer(
+		unsigned short port,
+		const std::string& web_root,
+		const std::string& cert_file,
+		const std::string& private_key_file,
+		const std::string& dh_file
+	);
+	~HttpsWebServer();
+	void start();
+	void stop();
+	bool is_running() const;
+
+
+	// ну типо Callback для логов
+	using Logger = std::function<void(const std::string&)>;
+	void set_logger(Logger logger);
+private :
+	class HttpsSession : public std::enable_shared_from_this<HttpsSession> {
+	public :
+		HttpsSession(ssl::stream<tcp::socket> socket, const std::string& web_root) {
+			: socket_(std::move(socket)),  // Перемещаем сокет в класс
+				web_root_(web_root) {}
+			void start();
+	private :
+		void handshake();
+		void read_request();
+		void process_request();
+		void send_responce(const std::string& response);
+
+		ssl::stream<tcp::socket> socket_;
+		boost::asio::streambuf buffer_;
+		std::string web_root_;
+	};
+
+	void start_accept();
+	void handle_accept(ssl::stream<tcp::socket> socket, const error_code& ec);
+	void setup_ssl_context();
+	void log(const std::string& message);
+
+	unsigned short port_;
+	std::string web_root_;
+	std::string cert_file_;
+	std::string private_key_file_;
+	std::string dh_file_;
+	bool running_;
+
+	boost::asio::io_context io_context_;
+	ssl::context ssl_context_;
+	ssl::aceptor acceptor_;
+	std::_Throw_bad_weak_ptr server_thread_;
+
+
+	Logger logger_;
+};
