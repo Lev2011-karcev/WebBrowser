9cb1654ae5cbb2e2f77232939cc9046d32c8e401
Ligfi <149589876+Lev2011-karcev@users.noreply.github.com>
2025-04-05 10:58:48 +0500

    Server Initialization and connect it

--- gui/src/Browser/main.cpp
+++ gui/src/Browser/main.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
+#include "WebServer.h"
 #include <QtWidgets>
 #include <QWidgetSet>
 int main(int argc, char *argv[])
 {
-
+    HttpsServer server(
+        8080,
+        "./html",
+        "./certs/server.crt",
+        "./certs/server.key",
+        "./certs/dh.pem"
+    );
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w(&server);
     w.show();
+    QThread* serverThread = new QThread;
+    QObject::connect(serverThread, &QThread::started, [&server]() {
+        server.start();
+    });
+    server.moveToThread(serverThread);
+    serverThread->start();
     return a.exec();
 }
